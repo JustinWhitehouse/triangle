db8d290fcef968c6a19cf6a830d0646a03d28c6e
JustinWhitehouse <justin.t.whitehouse@gmail.com>
2021-05-03 14:24:50 +0100

    Now draws Siepinski gasket

--- triangle.py
+++ triangle.py
@@ -2,12 +2,45 @@ import turtle
 
 DEFAULT_SIDE = 10
 
-
 def draw_triangle(pen, l):
 	for i in range(0,3):
 		pen.forward(l)
 		pen.left(120)
+
 	return None
+
+def move_to_start(pen, l, i):
+
+	pen.penup()
+
+	if i<=0:
+		pass
+
+	else:
+		move_to_start(pen,l,i-1)
+		
+		pen.left(120)
+		pen.forward(l * 2**(i-1))
+		pen.right(180)
+
+	return i
+
+def draw_generation(pen, l, i):
+	# generation = i
+
+	pen.pendown()
+
+	if i <= 0:
+		draw_triangle(pen, l)
+
+	else:
+		for n in range(0,3):
+			draw_generation(pen, l, i-1)
+			pen.forward(2**i * l)
+			pen.left(120)
+
+	return i
+
 
 def draw_recursive(pen, l, N, i=0):
 	
@@ -28,12 +61,23 @@ def draw_recursive(pen, l, N, i=0):
 
 def main():
 
-	turtle.clear()
+	generations = 4
+
+
 	my_window = turtle.Screen()
+	my_window.clear()
 	my_pen = turtle.Turtle()
+	my_pen.speed(0)
+
 
 	#draw_triangle(my_pen, DEFAULT_SIDE)
-	draw_recursive(my_pen, DEFAULT_SIDE, 4)
+	#draw_recursive(my_pen, DEFAULT_SIDE, 4)
+	move_to_start(my_pen, DEFAULT_SIDE, generations)
+	draw_generation(my_pen, DEFAULT_SIDE, generations)
+
+	deleteme = raw_input("press any key to exit")
+
+	return None
 
 if __name__ == "__main__":
 	main()
