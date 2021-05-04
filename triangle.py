0b4cb20d81713bd3a6335514a4a02df0416fa759
John <eddaardvark@yahoo.co.uk>
2021-05-04 17:21:54 +0100

    Added some colour
    Made the line length scale with the number of generations
    Some optimisations

--- triangle.py
+++ triangle.py
@@ -1,6 +1,7 @@
 import turtle
 
-DEFAULT_SIDE = 10
+ 
+
 
 def draw_triangle(pen, l, rotate=0):
 	# triangle or triangle-like motif!
@@ -8,16 +9,21 @@ def draw_triangle(pen, l, rotate=0):
 
 	for i in range(0,3):
 		if i == R:
-			pen.forward(l/2)
+			l2 = l/2
+			pen.forward(l2)
 			pen.left(60)
-			pen.forward(l/2)
-			pen.backward(l/2)
+			pen.forward(l2)
+			pen.penup()
+			pen.backward(l2)
 			pen.right(60)
-			pen.forward(l/2)
+			pen.pendown()
+			pen.forward(l2)
 
 		else:
+			pen.color((1,0,0))
 			pen.forward(l)
-			
+			pen.color((0,0,0))
+
 		pen.left(120)
 
 	return None
@@ -47,19 +53,23 @@ def draw_generation(pen, l, i, r=0):
 		draw_triangle(pen, l, r)
 
 	else:
+		l3 = 2**i * l;
+		
 		for n in range(0,3):
 			draw_generation(pen, l, i-1, r-n)
-			pen.forward(2**i * l)
+			pen.forward(l3)
 			pen.left(120)
 
 		# now fill in centre
-		pen.forward(2**(i-1) * l)
+		l3 = l3 / 2
+		pen.forward(l3)
 		pen.left(60)
 		draw_generation(pen, l, i-1, r)
 
 		# restore position
 		pen.right(60)
-		pen.backward(2**(i-1) * l)
+		pen.penup();
+		pen.backward(l3)
 
 
 	return i
@@ -69,6 +79,7 @@ def draw_recursive(pen, l, N, i=0):
 	
 	# draw
 	if i <= N:
+		pen.pendown()
 
 		big_L = 2**i * l
 		draw_triangle(pen, big_L)
@@ -84,17 +95,17 @@ def draw_recursive(pen, l, N, i=0):
 
 def main():
 
-	generations = 6
-
+	generations = 3
+	DEFAULT_SIDE = 2 ** (10 - generations)
 	my_window = turtle.Screen()
 	my_window.clear()
 	my_pen = turtle.Turtle()
 	my_pen.speed(0)
-
+	my_pen.hideturtle()
 	move_to_start(my_pen, DEFAULT_SIDE, generations)
 	draw_generation(my_pen, DEFAULT_SIDE, generations)
 
-	deleteme = raw_input("press any key to exit")
+	#deleteme = raw_input("press any key to exit")
 
 	return None
 
